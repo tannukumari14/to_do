5cb36ccd4368ec9676702ef2cf487edeb45e0bf9
shivani mehta <shivanimehta21@navgurukul.org>
2023-11-01 10:51:11 +0530

    delete and done complete

---
+++ app/Status.js
@@ -0,0 +1,16 @@
+const Status=()=>{
+    return(
+        <div>
+            <ul>
+                <li><a>DONE</a></li>
+                <li><a>NOT DONE</a></li>
+                <li><a>PRIORITY</a></li>
+                <li><a>ALL</a></li>
+            </ul>
+
+        </div>
+
+    )
+
+}
+export default Status;
--- app/page.js
+++ app/page.js
@@ -1,18 +1,14 @@
+"use client"
 import React from "react";
-import Form from "./Form";
-
 import "./globals.css";
+import Functionality from "./Try";
 
-
-const page=()=>{
-	return (
-		<div>
-			<Form />
-			
-			
-		</div>
-	)
+const page = () => {
+  return (
+    <div>
+      <Functionality />
+    </div>
+  );
 }
 
 export default page;
-
